a8a36d2fbd34a972f1d8b7da3b07958e906640f2
Andrew Bell <fiixed@gmail.com>
2023-05-15 11:33:14 -0500

    POST frontend request

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
-  destination: 'Kepler-442 b',
+  target: 'Kepler-442 b',
   customer: ['NASA', 'ZTM'],
   upcoming: true,
   success: true,
@@ -34,5 +34,5 @@ function addNewLaunch(launch) {
 
 module.exports = {
   getAllLaunches,
-  addNewLaunch
+  addNewLaunch,
 };
